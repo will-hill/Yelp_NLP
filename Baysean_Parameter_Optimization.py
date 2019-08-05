973e9c16583a3585349bfed72a3c4fb3f9128814
William Hill <william.c.hill@gmail.com>
2019-08-05 02:51:47 +0000

    a few more runs

--- Baysean_Parameter_Optimization.py
+++ Baysean_Parameter_Optimization.py
@@ -1,8 +1,8 @@
 import time
 
 # REVIEW_FILE_CSV = 'reviews.csv'
+# SHUFFLED_REVIEW_FILE_CSV = 'shuffled.100000.reviews.csv'  # 'shuffled.reviews.csv'
 SHUFFLED_REVIEW_FILE_CSV = 'shuffled.100000.reviews.csv'  # 'shuffled.reviews.csv'
-SHUFFLED_REVIEW_FILE_CSV = 'shuffled.1000000.reviews.csv'  # 'shuffled.reviews.csv'
 #SHUFFLED_REVIEW_FILE_CSV = 'shuffled.reviews.csv'  # 'shuffled.reviews.csv'
 import pandas as pd
 
